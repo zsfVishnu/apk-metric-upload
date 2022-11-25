f5cb4dfcf4655feb61e7f037f35c984b40802ee7
zsfVishnu <vishnu.tp32@gmail.com>
2022-11-25 18:57:37 +0530

    feat: added ls

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ import {
 try {
   const flavorToBuild = getInput("flavor");
   const dir = getInput("working-directory");
+  console.log(execSync(`ls`, { encoding: "utf-8" }));
+
   handleWorkingDir(dir);
+  console.log(execSync(`ls`, { encoding: "utf-8" })); //handle flavor casing
+  console.log(execSync(`pwd`, { encoding: "utf-8" })); //handle flavor casing
   const pascalFlavour = getPascalCase(flavorToBuild);
   if (pascalFlavour === 0) {
     let err = new Error("Error with building flavor");
