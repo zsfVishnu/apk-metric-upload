375cbcc0790ba9186be31a7ce35bcc30bb1cb79e
Yasir <yasir.ansari@dream11.com>
2023-06-19 02:32:57 +0530

    chore: changed metric name

--- dist/index.js
+++ dist/index.js
@@ -9620,7 +9620,7 @@ const artifact = __nccwpck_require__(6954);
 
 function uploadArtifact(s0) {
   const artifactClient = artifact.create();
-  const artifactName = "metric-artifact";
+  const artifactName = "metric-artifact-new";
   const files = [`metric.json`];
   const rootDirectory = `.`;
   const options = {
